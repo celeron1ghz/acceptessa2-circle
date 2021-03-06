4b0d4472ff18fc5a4ef251b47ea64485192daa60
celeron1ghz <celeron1ghz@gmail.com>
2021-03-06 17:02:00 +0900

    impl

--- server/register/handler.js
+++ server/register/handler.js
@@ -1,28 +1,69 @@
 'use strict';
 
+const aws = require('aws-sdk');
+const ddb = new aws.DynamoDB.DocumentClient();
+const r = require('rand-token');
+
+const TABLE = "acceptessa2-circle-register";
+
+function log(event, status, error) {
+  console.log({
+    ...event.requestContext.http,
+    time: event.requestContext.timeEpoch,
+    apiId: event.requestContext.apiId,
+    status,
+    error,
+  });
+}
+
 module.exports.validate_mail = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'validate',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  const param = event.queryStringParameters || {};
+  const mail = param.mail;
+
+  if (!mail) {
+    log(event, "no_input");
+    return { statusCode: 403, body: "error" };
+  }
+
+  if (!mail.match(/@/)) {
+    log(event, "invalid_input");
+    return { statusCode: 403, body: "error" };
+  }
+
+  const token = r.generate(128);
+  const ret = await ddb
+    .put({ TableName: TABLE, Item: { token, mail } })
+    .promise()
+    .catch(err => err);
+
+  if (ret instanceof Error) {
+    log(event, "dynamodb_error", ret);
+    return { statusCode: 403, body: "error" };
+  }
+
+  log(event, "success");
+  return { statusCode: 200, body: "OK" };
 };
 
 module.exports.check_mail = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'check',
-      },
-      null,
-      2
-    ),
-  };
+  const param = event.queryStringParameters || {};
+  const token = param.t;
+
+  if (!token || !token.length) {
+    log(event, "no_input");
+    return { statusCode: 403, body: "error" };
+  }
+
+  const ret = await ddb
+    .get({ TableName: TABLE, Key: { token } })
+    .promise()
+    .catch(err => err);
+
+  if (!ret.Item) {
+    log(event, "no_item", ret);
+    return { statusCode: 403, body: "error" };
+  }
+
+  log(event, "success");
+  return { statusCode: 200, body: "OK" };
 };
